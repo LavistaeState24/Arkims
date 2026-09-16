f6f6a0a6df2a943826256aa5bf7fb1fea2ba5762
Lavista Estate <lavistaestates24@gmail.com>
2026-09-16 16:30:16 +0530

    home page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html", "./components/**/*.html", "./js/**/*.js"],
   theme: {
     extend: {
       colors: {
-        pine: { 950: '#011735', 800: '#022f6a', 700: '#022f6a', 500: '#022f6a', 100: '#E6EFF8' },
-        sand: { 600: '#cf9a55', 500: '#c89f53',400:'#e4be7a', 100: '#F9F4E8' },
+        pine: {
+          950: '#1d4b48', // Darkest background (Deep Teal/Pine)
+          800: '#235956', // Very dark pine
+          700: '#2c6d69', // Dark pine
+          500: '#3e8f8a', // Mid-tone pine
+          100: '#E6EFF8'  // Light background tint
+        }, sand: { 600: '#cf9a55', 500: '#c89f53', 400: '#e4be7a', 100: '#F9F4E8' },
         paper: '#F5F6F1',
+        darkBg: '#1d4b48',
+        LightBg: '#fefce8',
         ink: { DEFAULT: '#022f6a', soft: '#4B564E' },
         line: '#DDE3DC'
       },
